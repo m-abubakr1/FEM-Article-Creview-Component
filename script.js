97432c9841f8bd96c2f0f663dd0de58acfd2a074
M. Abubakr <mabubakr429@gmail.com>
2025-02-01 16:17:13 +0500

    Update Script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+const shareOption = document.querySelector('.share-option');
+
+document.querySelector('.share-button').addEventListener('click', function () {
+  this.classList.toggle('active');
+  shareOption.classList.toggle('active');
+});
